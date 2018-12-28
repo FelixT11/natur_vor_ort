20d97fb39dfaf7d3b7dcc1b0bb5c455e12679271
FelixT11 <felix@goldtalla.de>
2018-12-28 17:43:49 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $.backstretch([
+      "img/natur_1.jpg"
+    , "img/natur_2.jpg"
+    , "img/natur_3.jpg"
+    , "img/natur_4.jpg"
+    , "img/natur_5.jpg"
+    , "img/natur_6.jpg"
+    , "img/natur_7.jpg"
+  ], {
+        duration: 3000,
+        fade: 750
+    });
+});
